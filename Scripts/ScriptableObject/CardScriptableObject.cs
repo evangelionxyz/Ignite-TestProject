03fee3cb03c76a4773cffe7f9e83f63b426d3509
Evangelion Manuhutu <evangelion11.m@gmail.com>
2026-05-10 03:05:30 +0700

    Test scriptable object

---
+++ Scripts/ScriptableObject/CardScriptableObject.cs
@@ -0,0 +1,19 @@
+﻿using Ignite;
+
+namespace TestProject;
+
+[CreateAssetMenu(FileName = "CardScriptableObject", MenuName = "Card/Config")]
+public class CardScriptableObject : ScriptableObject
+{
+    public enum CardType
+    {
+        Default = 0,
+        Type_A,
+        Type_B,
+    }
+
+    [SerializeField] public string name;
+    [SerializeField] public int id;
+    // [SerializeField] public AssetHandle mesh;
+    [SerializeField] public CardType type;
+}
